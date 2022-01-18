a89839f038ac2953a5cf34e01fdb7df03182268a
Katerina Kreibich <87352324+Code-rina@users.noreply.github.com>
2022-01-18 15:09:59 -0800

    GET sign-up

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,19 +1,22 @@
 var express = require('express');
 const bcrypt = require('bcryptjs')
-const { User } = require('../models');
+const { User } = require('../db/models');
 const { getUserToken } = require('../auth');
+const { csrfProtection, asyncHandler } = require('./utils')
+
 
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/signup', (req, res) => {
-  const user = User.build();
-  res.render('sign-up', { title: "signup", user })
+router.get('/signup', csrfProtection, (async(req, res) => {
+  const user = await User.build();
+  res.render('sign-up', { title: "signup", user, body: {}, csrfToken: req.csrfToken(), })
   // res.send('respond with a resource');
-});
+}));
 
-router.post('/signup', async(req, res) => {
+// router.post('/signup', async(req, res) => {
 
-});
+// });
 
 module.exports = router;
