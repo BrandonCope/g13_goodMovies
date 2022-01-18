402635262375882c9b234c05cb78910471092176
brandon <bcopeland97@gmail.com>
2022-01-18 16:24:36 -0500

    setup for auth token

---
+++ express-project-starter/auth.js
@@ -0,0 +1,22 @@
+const { jwtConfig } = require('./config');
+const { secret, expiresIn } = jwtConfig;
+const { User } = require('./db/models')
+const jwt = require('jsonwebtoken');
+const bearerToken = require('express-bearer-token');
+
+const getUserToken = (user) => {
+    const userDataForToken = {
+        id: user.id,
+        email: user.email,
+    }
+
+    const token = jwt.sign(
+        { data: userDataForToken },
+        secret,
+        { expiresIn: parseInt(expiresIn, 10) }
+    )
+
+    return token;
+}
+
+module.exports = { getUserToken }
