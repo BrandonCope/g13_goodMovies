70235fdb3bfc71aba84bdb01e3bfa88fd115bc02
Mia <mia.joubert@gmail.com>
2022-01-24 08:47:24 -0600

    changes on Monday

--- routes/movies.js
+++ routes/movies.js
@@ -10,10 +10,37 @@ const router = express.Router()
 
 router.get('/',
   asyncHandler(async (req, res) => {
-    const movies = await Movie.findAll({
+    const aniMovies = await Movie.findAll({
+      where: {
+        genre: "Animation"
+      },
       order: [['movie_title', 'ASC']]
     });
-    return res.render('movies-all', { title: 'All Movies', movies })
+    const actionMovies = await Movie.findAll({
+      where: {
+        genre: "Action"
+      },
+      order: [['movie_title', 'ASC']]
+    });
+    const romComMovies = await Movie.findAll({
+      where: {
+        genre: "Rom-Com"
+      },
+      order: [['movie_title', 'ASC']]
+    });
+    const sciFiMovies = await Movie.findAll({
+      where: {
+        genre: "Sci-Fi"
+      },
+      order: [['movie_title', 'ASC']]
+    });
+    const topTenMovies = await Movie.findAll({
+      where: {
+        genre: "Top-Ten"
+      },
+      order: [['movie_title', 'ASC']]
+    });
+    return res.render('movies-all', { title: 'All Movies', aniMovies, actionMovies, sciFiMovies, romComMovies, topTenMovies })
   }))
 
 
@@ -24,7 +51,6 @@ router.get('/:id(\\d+)',
     const movieId = parseInt(req.params.id, 10);
     const movie = await Movie.findByPk(movieId)
     const userId = req.session.auth.userId
-
 
     const reviews = await Review.findAll({
       where: {
@@ -41,7 +67,6 @@ router.get('/:id(\\d+)',
       include: User
     })
 
-
     const rating = await Rating.findOne({
       where:
       {
@@ -50,11 +75,6 @@ router.get('/:id(\\d+)',
       }
     })
 
-    // const ratingUserId = rating.user_id
-    // console.log(userId)
-    // console.log(ratingUserId)
-
-    // console.log("!!!!!!!!!!!!!!", rating)
     let ratingId;
     if (!rating) {
       ratingId = 0
@@ -64,11 +84,13 @@ router.get('/:id(\\d+)',
 
     let sumRating = 0;
     let count = 0;
+    let avgRating = 0;
+
     ratings.forEach((el) => {
       let rate = el.rating
       sumRating += rate;
       count++;
-      avgRating = Math.floor(sumRating / count)
+      avgRating = (sumRating / count).toFixed(1)
     })
 
     res.render('movie-detail', { title: 'Movie Detail', movie, reviews, avgRating, rating, ratingId, csrfToken: req.csrfToken(), movieId, userId })
